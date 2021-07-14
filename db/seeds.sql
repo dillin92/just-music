a38848b734e5506281d619aa12806093f6103bf0
kdchurch1994 <kdchurch@bsu.edu>
2021-07-14 18:58:54 -0400

    initial database files created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO account_info (first_name, last_name, user_type, user_name, email_address)
+VALUES
+    ('Kyle', 'Church', 'member', 'kchurch', 'kdchurch@bsu.edu'),
+    ('Elizabeth', 'Sherrow', 'musician', 'esherrow', 'esherrow@gmail.com'),
+    ('Dillin', 'Helsley', 'producer', 'dhelsley', 'dhelsley@gmail.com');
