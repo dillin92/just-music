848d883f6f3ecdfca50caac47e29280de70e84ac
kdchurch1994 <kdchurch@bsu.edu>
2021-07-21 15:55:05 -0400

    Home-Routes and Login/Logout ROutes complete

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes')
-const sequelize = require('./config/connection');
-//const path = require('path');
-//const exphbs = require('express-handlebars');
-//const { Sequelize } = require('sequelize/types');
-//const hbs = exphbs.create({});
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const sess = {
+        secret: 'Super secret secret',
+        cookie: {},
+        resave: false,
+        saveUninitialized: true,
+        store: new SequelizeStore({
+                db: sequelize
+        })
+};
 
+app.use(session(sess));
 
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+//const { Sequelize } = require('sequelize/types');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Express middlware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use apiRoutes
-app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
