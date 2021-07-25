2872053be92cd938849f8438106548c27a2ef42a
dillin92 <dillin970@gmail.com>
2021-07-25 19:15:29 -0400

    updated models and fixed typos

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment')
+const Comment = require('./Comment');
+const Concerts = require('./Concerts');
+// const Artist = require('./topArtists');
+const TopArtists = require('./topArtists');
 
 // const Comment = require('./Comment');
 
@@ -37,4 +40,20 @@ Post.hasMany(Comment,{
     foreignKey:'post_id'
 });
 
-module.exports = { User, Post, Comment};
+// User.hasMany(Concerts, {
+//     foreignKey: 'user_id'
+// });
+
+// Concerts.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
+
+// User.hasMany(TopArtists, {
+//     foreignKey: 'user_id'
+// });
+
+// TopArtists.belongsTo(User, {
+//     foreignKey: 'post_id'
+// });
+
+module.exports = { User, Post, Comment };
--- models/topArtists.js
+++ models/topArtists.js
@@ -1,35 +1,45 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class TopArtists extends Model {}
+// class TopArtists extends Model {}
 
-TopArtists.init(
-    {
-        id: {
-            type: dataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        artist: {
-            artistName: DataTypes.STRING,
-            artistImg: DataTypes.STRING,
-            allowNull: true
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'TopArtists'
-    }
-);
+// TopArtists.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
 
-moodule.exports = TopArtists;
+//          artist_name: {
+//                 type: DataTypes.STRING,
+//                 allowNull: false,
+//                 validate: {
+//                     len: [1]
+//                 }
+
+
+//             },
+
+//         // artist_img: {
+//         //     type: DataTypes.,
+//         //     allowNull: false
+//         // },
+//         user_id: {
+//             type: DataTypes.INTEGER,
+//             references: {
+//                 model: 'user',
+//                 key: 'id'
+//             }
+//         }
+//     },
+//     {
+//         sequelize,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'TopArtists'
+//     }
+// );
+
+// module.exports = TopArtists;
