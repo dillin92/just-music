dc9b2044df4ee685b4c79f8e28d99516cfb7f7e2
kdchurch1994 <kdchurch@bsu.edu>
2021-07-25 22:12:12 -0400

    updated index.js in controllers to include dashboard-routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashboaredRoutes = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboaredRoutes);
 router.use('/api', apiRoutes);
+
 
 router.use((req, res) => {
     res.status(404).end();
