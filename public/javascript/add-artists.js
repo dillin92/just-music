512c336567d2ccbc852290bf86f5184c4bf72d08
kellydhunter1 <kellydhunter1@gmail.com>
2021-07-25 22:46:34 -0400

    added handlebars to profile page for artists, concerts, and posts"

--- public/javascript/add-artists.js
+++ public/javascript/add-artists.js
@@ -9,8 +9,8 @@ $("#search-artist").click(function() {
     fetch('https://www.theaudiodb.com/api/v1/json/1/search.php?s=' + artistId +'').then(function(response) {
         response.json().then(function(data) {
             const artistCard = $("<div>").addClass("card col s3");
-            const artistName = $("<p>").addClass(".card-title").text(data.artists[0].strArtist);
-            const artistImg = $("<div>").addClass(".card-image").append('<img class ="responsive-img" src="' + data.artists[0].strArtistThumb + '/preview"/>');
+            const artistName = $("<p>").addClass("card-title").text(data.artists[0].strArtist);
+            const artistImg = $("<div>").addClass("card-image").append('<img class ="responsive-img" src="' + data.artists[0].strArtistThumb + '/preview"/>');
 
             $("#top-artists").append(artistCard)
             $(artistCard).append(artistName, artistImg);
